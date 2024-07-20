14ca12c20e6c64e3287bbe9729034fde311067ad
Tracy Karanja <chirutracy@gmail.com>
2024-07-20 10:15:28 +0300

    integrated shecodes api

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
-function generateRecipe(event){
-    event.preventDefault()
+function displayRecipe(response) {
+  console.log("recipe generated");
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
 
-    new Typewriter("#recipe", {
-      strings: 'Here is the recipe for cake',
-      autoStart: true,
-      delay: 40,
-      cursor: "|"
-    });
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructions = document.querySelector("#user-instructions").value;
+  let apiKey = "16t1b3fa04b8866116ccceb0d2do3a04";
+  let prompt = `User instructions are: Generate a recipe for ${instructions}`;
+  let context =
+    "You are an expert at recipes. Your mission is to generate a short and easy recipe in basic HTML. Make sure to follow user instructions. Sign the recipe at the end with '<strong>SheCodes AI</strong>' in bold";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt
+  }&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-generator-form");
-recipeForm.addEventListener('submit', generateRecipe)
+recipeForm.addEventListener("submit", generateRecipe);
